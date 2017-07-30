c6774b330989190f6b103814d0c7a65c978416ef
ClarkNan <30437483+ClarkNan@users.noreply.github.com>
2017-07-30 23:44:55 +0800

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,12 @@
+// 燒烤計算測試
+
+document.getElementById('countId').onclick = function (){
+	var light = 888 + (888*0.1);
+	var hight = 489 + (489*0.1);
+	var soso = 429 + (429*0.1);
+	var lightPrice = parseInt(document.getElementById('lightId').value)*light;
+	var hightPrice = parseInt(document.getElementById('hightId').value)*hight;
+	var sosoPrice = parseInt(document.getElementById('sosoId').value)*soso;
+	document.getElementById('totalId').textContent = lightPrice + hightPrice + sosoPrice
+	document.getElementById('totalId').textContent = lightPrice 
+}
